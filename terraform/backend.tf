b0999cea4d7b73b8ec00ce53ed5b41900dc6cd08
vamsi <vamsikrishna1419@gmail..com>
2026-06-08 15:55:35 +0530

    tf fmt

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
     bucket         = "purely-terraform-state" # Set via -backend-config in CI/CD
-    key            = "demo" # Set via -backend-config in CI/CD
-    region         = "us-east-1" # Set via -backend-config in CI/CD
+    key            = "demo"                   # Set via -backend-config in CI/CD
+    region         = "us-east-1"              # Set via -backend-config in CI/CD
     dynamodb_table = "purely-terraform-locks" # Set via -backend-config in CI/CD
     encrypt        = true
   }
