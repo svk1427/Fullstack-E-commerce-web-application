ad7722cc25c000170c8d36140ab1144810095f0d
vamsi <vamsikrishna1419@gmail..com>
2026-07-18 16:39:19 +0530

    tf code format

--- terraform/us-east-1/c1-versions.tf
+++ terraform/us-east-1/c1-versions.tf
@@ -6,17 +6,17 @@ terraform {
       source = "hashicorp/aws"
       #version = ">= 4.65"
       version = ">= 5.31"
-     }
+    }
   }
   # Adding Backend as S3 for Remote State Storage
   backend "s3" {
     bucket = "terraform-on-aws-eks"
     key    = "dev/eks-cluster/terraform.tfstate"
-    region = "us-east-1" 
- 
+    region = "us-east-1"
+
     # For State Locking
-    dynamodb_table = "dev-ekscluster"    
-  }  
+    dynamodb_table = "dev-ekscluster"
+  }
 }
 
 # Terraform Provider Block
