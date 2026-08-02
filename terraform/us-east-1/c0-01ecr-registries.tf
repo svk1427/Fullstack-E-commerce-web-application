fa58f59799b15525f8c031ee9b0df1f32a821f6f
Sanaboyina, VamsiKrishna <vamsikrishna.sanaboyina@broadridge.com>
2026-08-02 17:38:18 +0530

    Add force_delete=true to ECR repositories for clean destroy

--- terraform/us-east-1/c0-01ecr-registries.tf
+++ terraform/us-east-1/c0-01ecr-registries.tf
@@ -3,11 +3,13 @@
 # =============================================================================
 # This file creates ECR repositories for all microservices
 # Path Filter Test: Changes here should ONLY trigger ci-cd-tf-manage.yml
+# force_delete = true allows terraform destroy to delete repos with images
 # =============================================================================
 
 resource "aws_ecr_repository" "purely_service_registry" {
   name                 = "purely_service_registry"
   image_tag_mutability = "MUTABLE"
+  force_delete         = true
 
   image_scanning_configuration {
     scan_on_push = true
@@ -17,6 +19,7 @@ resource "aws_ecr_repository" "purely_service_registry" {
 resource "aws_ecr_repository" "purely_category_registry" {
   name                 = "purely_category_registry"
   image_tag_mutability = "MUTABLE"
+  force_delete         = true
 
   image_scanning_configuration {
     scan_on_push = true
@@ -26,6 +29,7 @@ resource "aws_ecr_repository" "purely_category_registry" {
 resource "aws_ecr_repository" "purely_gateway_registry" {
   name                 = "purely_gateway_registry"
   image_tag_mutability = "MUTABLE"
+  force_delete         = true
 
   image_scanning_configuration {
     scan_on_push = true
@@ -35,6 +39,7 @@ resource "aws_ecr_repository" "purely_gateway_registry" {
 resource "aws_ecr_repository" "purely_web_registry" {
   name                 = "purely_web_registry"
   image_tag_mutability = "MUTABLE"
+  force_delete         = true
 
   image_scanning_configuration {
     scan_on_push = true
@@ -44,6 +49,7 @@ resource "aws_ecr_repository" "purely_web_registry" {
 resource "aws_ecr_repository" "purely_product_registry" {
   name                 = "purely_product_registry"
   image_tag_mutability = "MUTABLE"
+  force_delete         = true
 
   image_scanning_configuration {
     scan_on_push = true
@@ -53,6 +59,7 @@ resource "aws_ecr_repository" "purely_product_registry" {
 resource "aws_ecr_repository" "purely_auth_registry" {
   name                 = "purely_auth_registry"
   image_tag_mutability = "MUTABLE"
+  force_delete         = true
 
   image_scanning_configuration {
     scan_on_push = true
@@ -62,6 +69,7 @@ resource "aws_ecr_repository" "purely_auth_registry" {
 resource "aws_ecr_repository" "purely_order_registry" {
   name                 = "purely_order_registry"
   image_tag_mutability = "MUTABLE"
+  force_delete         = true
 
   image_scanning_configuration {
     scan_on_push = true
@@ -71,6 +79,7 @@ resource "aws_ecr_repository" "purely_order_registry" {
 resource "aws_ecr_repository" "purely_cart_registry" {
   name                 = "purely_cart_registry"
   image_tag_mutability = "MUTABLE"
+  force_delete         = true
 
   image_scanning_configuration {
     scan_on_push = true
@@ -80,6 +89,7 @@ resource "aws_ecr_repository" "purely_cart_registry" {
 resource "aws_ecr_repository" "purely_notification_registry" {
   name                 = "purely_notification_registry"
   image_tag_mutability = "MUTABLE"
+  force_delete         = true
 
   image_scanning_configuration {
     scan_on_push = true
@@ -89,6 +99,7 @@ resource "aws_ecr_repository" "purely_notification_registry" {
 resource "aws_ecr_repository" "purely_user_registry" {
   name                 = "purely_user_registry"
   image_tag_mutability = "MUTABLE"
+  force_delete         = true
 
   image_scanning_configuration {
     scan_on_push = true
